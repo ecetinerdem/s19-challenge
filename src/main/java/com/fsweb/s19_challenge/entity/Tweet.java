0496fe6f9d96658371931e5d60f3d58cfc90aa34
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-01 01:52:49 +0400

    Update Tweet.java

--- src/main/java/com/fsweb/s19_challenge/entity/Tweet.java
+++ src/main/java/com/fsweb/s19_challenge/entity/Tweet.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,26 +27,26 @@ public class Tweet {
     @Column(name = "id")
     private Long id;
 
-
     @Column(name = "content")
     @NotBlank(message = "Tweet cannot be blank")
     @NotEmpty(message = "Tweet cannot be empty")
     @NotNull(message = "Tweet cannot be null")
-    @Size(max = 250, message = "Tweet cannot be longer than 250 character")
+    @Size(max = 250, message = "Tweet cannot be longer than 250 characters")
     private String content;
 
+    @Column(name = "is_liked", nullable = false, columnDefinition = "boolean default false")
+    private Boolean isLiked = false;
+
     @Column(name = "created_at")
-    @Timestamp
-    LocalDate createdAt;
+    @Temporal(TemporalType.DATE)
+    private LocalDate createdAt;
 
     @JsonBackReference
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH,CascadeType.PERSIST, CascadeType.REFRESH})
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "user_id")
     private User user;
 
     @JsonManagedReference
     @OneToMany(mappedBy = "tweet", cascade = CascadeType.ALL)
     private List<Comment> comments = new ArrayList<>();
-
-
 }
