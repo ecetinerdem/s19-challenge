35c77470edce4e9f94f50d421906e78c10291959
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-01 01:50:53 +0400

    Update Comment.java

--- src/main/java/com/fsweb/s19_challenge/entity/Comment.java
+++ src/main/java/com/fsweb/s19_challenge/entity/Comment.java
@@ -1,13 +1,11 @@
 package com.fsweb.s19_challenge.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +13,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Entity
-@Table(schema = "fsweb",name = "comment")
+@Table(schema = "fsweb", name = "comment")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -30,20 +28,23 @@ public class Comment {
     @NotNull
     @NotEmpty
     @NotBlank
-    @Size(max = 250, message = "Comment cannot be longer than 250 character")
+    @Size(max = 250, message = "Comment cannot be longer than 250 characters")
     private String content;
 
+    @Column(name = "is_liked", nullable = false, columnDefinition = "boolean default false")
+    private Boolean isLiked = false;
+
     @Column(name = "created_at")
-    @Timestamp
+    @Temporal(TemporalType.DATE)
     private LocalDate createdAt;
 
     @JsonBackReference
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH,CascadeType.PERSIST, CascadeType.REFRESH})
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "tweet_id")
     private Tweet tweet;
 
     @JsonBackReference
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH,CascadeType.PERSIST, CascadeType.REFRESH})
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "user_id")
     private User user;
 }
