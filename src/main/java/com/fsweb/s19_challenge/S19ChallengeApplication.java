cc276b9dadde6732ec5eafde77bf2f5209d3ee85
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-31 22:34:51 +0400

    initilize s19

---
+++ src/main/java/com/fsweb/s19_challenge/S19ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.fsweb.s19_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S19ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S19ChallengeApplication.class, args);
+	}
+
+}
