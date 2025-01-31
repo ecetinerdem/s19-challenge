83f935d4ffec922c113382cac6a128d59cf36f30
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-01 00:04:14 +0400

    Update Tweet.java

--- src/main/java/com/fsweb/s19_challenge/entity/Tweet.java
+++ src/main/java/com/fsweb/s19_challenge/entity/Tweet.java
@@ -1,9 +1,17 @@
 package com.fsweb.s19_challenge.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(schema = "fsweb", name = "tweet")
@@ -16,5 +24,23 @@ public class Tweet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    
+
+
+    @Column(name = "content")
+    @NotBlank(message = "Tweet cannot be blank")
+    @NotEmpty(message = "Tweet cannot be empty")
+    @NotNull(message = "Tweet cannot be null")
+    @Size(max = 250, message = "Tweet cannot be longer than 250 character")
+    private String content;
+
+    @Column(name = "created_at")
+    @Timestamp
+    LocalDate createdAt;
+
+    @JsonBackReference
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH,CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
 }
