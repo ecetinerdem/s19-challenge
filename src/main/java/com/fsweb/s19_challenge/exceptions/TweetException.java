9135f999bb0a096bc1d086996d59638e8914cf42
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 00:32:36 +0400

    Create TweetException.java

---
+++ src/main/java/com/fsweb/s19_challenge/exceptions/TweetException.java
@@ -0,0 +1,19 @@
+package com.fsweb.s19_challenge.exceptions;
+
+import com.fsweb.s19_challenge.entity.Tweet;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TweetException extends RuntimeException {
+    private  HttpStatus httpStatus;
+    public TweetException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
