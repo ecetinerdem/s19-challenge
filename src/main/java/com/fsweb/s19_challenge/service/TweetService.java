76413e9acc90a87bbd0088229a2faad0d3857c16
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 00:13:38 +0400

    Create TweetService.java

---
+++ src/main/java/com/fsweb/s19_challenge/service/TweetService.java
@@ -0,0 +1,15 @@
+package com.fsweb.s19_challenge.service;
+
+import com.fsweb.s19_challenge.entity.Tweet;
+
+import java.util.List;
+
+public interface TweetService {
+    List<Tweet> findAllTweets();
+    List<Tweet> findTweetByUserId(Long id);
+    Tweet findTweetById(Long id);
+    Tweet saveTweet(Tweet tweet);
+    Tweet updateTweet(Long id, Tweet tweet);
+    Void deleteTweet(Long id);
+
+}
