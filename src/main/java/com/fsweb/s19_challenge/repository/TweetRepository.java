281230af65f8c3f1865283846f9418d19b02aaf8
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 00:00:32 +0400

    Update TweetRepository.java

--- src/main/java/com/fsweb/s19_challenge/repository/TweetRepository.java
+++ src/main/java/com/fsweb/s19_challenge/repository/TweetRepository.java
@@ -5,11 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
-    @Query("SELECT t FROM Tweet t WHERE t.id = :tweetId")
-    Optional<Tweet> findTweetById(@Param("tweetId") Long tweetId);
+    // Query to find all tweets by a specific user
+    @Query("SELECT t FROM Tweet t WHERE t.user.id = :userId")
+    List<Tweet> findAllTweetsByUserId(@Param("userId") Long userId);
 
+    // Query to find a single tweet by its ID and ensure it belongs to a specific user
+    @Query("SELECT t FROM Tweet t WHERE t.id = :tweetId AND t.user.id = :userId")
+    Optional<Tweet> findTweetByIdAndUserId(@Param("tweetId") Long tweetId, @Param("userId") Long userId);
 }
