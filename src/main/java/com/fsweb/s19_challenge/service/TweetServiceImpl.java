866ac74c040f9e854bec5707cd4614030fc564b7
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 00:19:24 +0400

    Create TweetServiceImpl.java

---
+++ src/main/java/com/fsweb/s19_challenge/service/TweetServiceImpl.java
@@ -0,0 +1,47 @@
+package com.fsweb.s19_challenge.service;
+
+import com.fsweb.s19_challenge.entity.Tweet;
+import com.fsweb.s19_challenge.repository.TweetRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class TweetServiceImpl implements TweetService{
+
+    private final TweetRepository tweetRepository;
+
+    @Override
+    public List<Tweet> findAllTweets() {
+        return tweetRepository.findAll();
+    }
+
+    @Override
+    public List<Tweet> findTweetByUserId(Long id) {
+        return tweetRepository.findAllTweetsByUserId(id);
+    }
+
+    @Override
+    public Tweet findTweetById(Long id) {
+        Tweet foundTweet = tweetRepository.findById(id).orElseThrow(() -> new TweetException("Tweet with given id does not exist " + id, HttpStatus.NOT_FOUND));
+        return null;
+    }
+
+    @Override
+    public Tweet saveTweet(Tweet tweet) {
+        return null;
+    }
+
+    @Override
+    public Tweet updateTweet(Long id, Tweet tweet) {
+        return null;
+    }
+
+    @Override
+    public Void deleteTweet(Long id) {
+        return null;
+    }
+}
