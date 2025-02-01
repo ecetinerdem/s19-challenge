5d31b9bcd68f569126a0431f90e9123c276cf5a7
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-01 12:15:10 +0400

    Update Like.java

--- src/main/java/com/fsweb/s19_challenge/entity/Like.java
+++ src/main/java/com/fsweb/s19_challenge/entity/Like.java
@@ -1,9 +1,12 @@
 package com.fsweb.s19_challenge.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(schema = "fsweb", name = "like")
@@ -16,5 +19,12 @@ public class Like {
     @Column(name = "id")
     private Long id;
 
-    
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE)
+    private LocalDate createdAt;
+
+    @JsonBackReference
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
 }
