ff6a6c6cfa9d718752e095986bf65a13776d97ee
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 00:39:43 +0400

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/fsweb/s19_challenge/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.fsweb.s19_challenge.exceptions;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<TweetErrorResponse> handleException(TweetException tweetException) {
+        log.error("A tweet error has occurred ", tweetException);
+        TweetErrorResponse tweetErrorResponse = new TweetErrorResponse(tweetException.getHttpStatus().value(), tweetException.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(tweetErrorResponse, tweetException.getHttpStatus());
+    }
+}
