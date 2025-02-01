1049f0e73d12876929eaf8c05aea88cc2814aa62
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 00:04:37 +0400

    Update LikeRepository.java

--- src/main/java/com/fsweb/s19_challenge/repository/LikeRepository.java
+++ src/main/java/com/fsweb/s19_challenge/repository/LikeRepository.java
@@ -1,4 +1,7 @@
 package com.fsweb.s19_challenge.repository;
 
-public interface LikeRepository {
+import com.fsweb.s19_challenge.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
 }
