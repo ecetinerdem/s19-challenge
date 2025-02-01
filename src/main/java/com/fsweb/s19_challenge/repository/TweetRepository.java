5ccd91bddd927cf8c670204b26a9eda97d2e7415
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-01 13:03:53 +0400

    Update TweetRepository.java

--- src/main/java/com/fsweb/s19_challenge/repository/TweetRepository.java
+++ src/main/java/com/fsweb/s19_challenge/repository/TweetRepository.java
@@ -1,4 +1,14 @@
 package com.fsweb.s19_challenge.repository;
 
-public interface TweetRepository {
+import com.fsweb.s19_challenge.entity.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+    @Query("SELECT t FROM Tweet t WHERE t.id = :tweetId")
+    Optional<Tweet> findTweetById(@Param("tweetId") Long tweetId);
 }
