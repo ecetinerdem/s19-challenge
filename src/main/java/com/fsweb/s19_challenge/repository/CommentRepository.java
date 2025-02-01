04a163ae752930cc99ef2a5cc693efaf35a0ccd8
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 00:04:38 +0400

    Update CommentRepository.java

--- src/main/java/com/fsweb/s19_challenge/repository/CommentRepository.java
+++ src/main/java/com/fsweb/s19_challenge/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.fsweb.s19_challenge.repository;
 
-public interface CommentRepository {
+import com.fsweb.s19_challenge.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
