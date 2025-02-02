1e658d4fa67469136ab34de8a2badb3d2e82e3ad
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 21:51:52 +0400

    Create UserServiceImpl.java

---
+++ src/main/java/com/fsweb/s19_challenge/service/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.fsweb.s19_challenge.service;
+
+import com.fsweb.s19_challenge.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username does not exist"));
+    }
+}
