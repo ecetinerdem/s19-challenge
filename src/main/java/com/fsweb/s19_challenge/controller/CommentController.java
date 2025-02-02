869231ee8e4181748edb9b25c865a6f50bb12e7d
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-03 00:07:24 +0400

    Update CommentController.java

--- src/main/java/com/fsweb/s19_challenge/controller/CommentController.java
+++ src/main/java/com/fsweb/s19_challenge/controller/CommentController.java
@@ -1,11 +1,28 @@
 package com.fsweb.s19_challenge.controller;
 
+import com.fsweb.s19_challenge.entity.Comment;
+import com.fsweb.s19_challenge.service.CommentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RequestMapping("/comment")
 @RestController
 public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping
+    public List<Comment> findAllComments() {
+        return commentService.findAllComments();
+    }
+
+    @GetMapping("/{id}")
+    public Comment findCommentById(@PathVariable("id") Long id) {
+        return commentService.findCommentById(id);
+    }
+
+    @PostMapping
+
 }
