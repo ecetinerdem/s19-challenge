f2b8215b662e818056d9009ba5faff1bdbd520d5
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-03 00:02:05 +0400

    Update GlobalExceptionHandler.java

--- src/main/java/com/fsweb/s19_challenge/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/fsweb/s19_challenge/exceptions/GlobalExceptionHandler.java
@@ -17,4 +17,11 @@ public class GlobalExceptionHandler {
         TweetErrorResponse tweetErrorResponse = new TweetErrorResponse(tweetException.getHttpStatus().value(), tweetException.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(tweetErrorResponse, tweetException.getHttpStatus());
     }
+
+    @ExceptionHandler
+    public ResponseEntity<CommentErrorResponse> handleException(CommentException commentException) {
+        log.error("A tweet error has occurred ", commentException);
+        CommentErrorResponse commentErrorResponse = new CommentErrorResponse(commentException.getHttpStatus().value(), commentException.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(commentErrorResponse, commentException.getHttpStatus());
+    }
 }
