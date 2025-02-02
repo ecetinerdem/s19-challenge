c34492335d33c6eb2ce444de8f64011ca04ece5a
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 21:51:54 +0400

    Update UserRepository.java

--- src/main/java/com/fsweb/s19_challenge/repository/UserRepository.java
+++ src/main/java/com/fsweb/s19_challenge/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package com.fsweb.s19_challenge.repository;
 
-import com.fsweb.s19_challenge.entity.Tweet;
+
 import com.fsweb.s19_challenge.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    Optional<User> findByUsername(String username);
 }
 
