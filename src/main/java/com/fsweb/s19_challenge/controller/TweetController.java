165586bf06053adc5d4ee0a6dbb7ea1918b6217e
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 20:53:17 +0400

    Update TweetController.java

--- src/main/java/com/fsweb/s19_challenge/controller/TweetController.java
+++ src/main/java/com/fsweb/s19_challenge/controller/TweetController.java
@@ -1,8 +1,10 @@
 package com.fsweb.s19_challenge.controller;
 
+import com.fsweb.s19_challenge.entity.Tweet;
 import com.fsweb.s19_challenge.service.TweetService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,16 @@ public class TweetController {
     private final TweetService tweetService;
 
     @GetMapping
-    public List<TweetResponse>
+    public List<Tweet> findAllTweets() {
+        return tweetService.findAllTweets();
+    }
+
+    @GetMapping("/{userId}")
+    public List<Tweet> findTweetByUserId(@PathVariable("userId") Long id) {
+        return tweetService.findTweetByUserId(id);
+    }
+
+    @GetMapping("/{tweetId}")
+    public Tweet findTweetById
+
 }
