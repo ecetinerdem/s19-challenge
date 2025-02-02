dc52b5e9f9ef54f679231bd0c108ca6e9e22495e
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-02 20:53:27 +0400

    Update Tweet.java

--- src/main/java/com/fsweb/s19_challenge/entity/Tweet.java
+++ src/main/java/com/fsweb/s19_challenge/entity/Tweet.java
@@ -9,7 +9,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
