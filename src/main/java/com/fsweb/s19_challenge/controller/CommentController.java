35c0057a3589ed9653b7d5feb2d622bd1c75a51d
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-03 00:04:22 +0400

    Create CommentController.java

---
+++ src/main/java/com/fsweb/s19_challenge/controller/CommentController.java
@@ -0,0 +1,11 @@
+package com.fsweb.s19_challenge.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RequestMapping("/comment")
+@RestController
+public class CommentController {
+}
